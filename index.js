55bb9450b8baa409b8b27ee2e7a4ee3e3c647ac6
zhouhanlong <2278633856@qq.com>
2017-10-13 16:27:26 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express'),
+  app = express();
+
+//设置静态资源目录
+app.use(express.static('static'));
+
+app.get('*', function (req, res) {
+  res.send('index.html');
+});
+
+app.listen(3000, function () {
+  console.log('server running at 3000!');
+});
